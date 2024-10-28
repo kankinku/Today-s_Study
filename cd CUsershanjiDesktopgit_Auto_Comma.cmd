a52229e73aca371dfa4352c8bda93a195940602f
Hanjinu <wlsdn1232580@gmail.com>
2024-10-28 09:45:25 +0900

    today study update

---
+++ cd CUsershanjiDesktopgit_Auto_Comma.cmd
@@ -0,0 +1,12 @@
+cd C:\Users\hanji\Desktop\git_Auto_Command
+del /Q *.txt
+
+set "today=%date:~0,4%-%date:~5,2%-%date:~8,2%"
+set "time=%time:~0,2%-%time:~3,2%-%time:~6,2%"
+set "time=%time: =0%"
+echo. > "%today%_%time%.txt"
+
+
+git add .
+git commit -m "today study update"
+git push -u origin main
